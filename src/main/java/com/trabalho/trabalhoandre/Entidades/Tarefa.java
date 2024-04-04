be2610e6f2ef09669432631b0782e325e61a1287
Dipirona537 <89006273+Dipirona537@users.noreply.github.com>
2024-04-04 16:28:03 -0400

    Adicionado tela

---
+++ src/main/java/com/trabalho/trabalhoandre/Entidades/Tarefa.java
@@ -0,0 +1,43 @@
+package com.trabalho.trabalhoandre.Entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Tarefa implements Serializable {
+    private String titulo;
+    private String desc;
+    private Date entrega;
+    private int prioridade;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getEntrega() {
+        return entrega;
+    }
+
+    public void setEntrega(Date entrega) {
+        this.entrega = entrega;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
